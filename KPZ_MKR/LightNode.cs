f42cfe8b0f6c90146ef9d958d1e86e1c3add7e97
Денис Бордюг <ipz224_bdv@student.ztu.edu.ua>
2026-03-31 18:02:26 +0300

    Add Template Method pattern for component lifecycle hooks

--- KPZ_MKR/LightNode.cs
+++ KPZ_MKR/LightNode.cs
@@ -4,5 +4,28 @@
     {
         public abstract string InnerHTML { get; }
         public abstract string OuterHTML { get; }
+
+        // --- ПАТЕРН ШАБЛОННИЙ МЕТОД ---
+
+        // Шаблонний метод, що задає алгоритм життєвого циклу
+        public string Render()
+        {
+            OnCreated();
+            OnStylesApplied();
+            OnTextRendered();
+
+            // Основна логіка отримання HTML (вже реалізована в дочірніх класах)
+            string html = OuterHTML;
+
+            OnRendered();
+
+            return html;
+        }
+
+        // Віртуальні хуки (порожні за замовчуванням)
+        protected virtual void OnCreated() { }
+        protected virtual void OnStylesApplied() { }
+        protected virtual void OnTextRendered() { }
+        protected virtual void OnRendered() { }
     }
 }
